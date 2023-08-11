fd72df301ba6fad9bae9e280c3b2f40064e90da3
Alexandra <sashatoropova@gmail.com>
2023-08-11 14:34:38 +0700

    add pr3

--- seminar_project_1/game_app/urls.py
+++ seminar_project_1/game_app/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('coinplay/', views.coin, name='index'),
-	path('cube/', views.dice, name='index'),
-	path('random/', views.rand_int, name='index'),
+	path('coinplay/<int:count>', views.coin, name='coin'),
+	path('cube/<int:count>', views.dice, name='cube'),
+	path('r_n/<int:count>', views.rand_int, name='random_number'),
+	path('articles/<int:pk>', views.get_articles_by_author, name='articles'),
+	path('article/<int:pk>', views.article, name='article'),
 ]
